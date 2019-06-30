8fe592f82fdbd77b182f47c40ad7e14db4c9ed52
BuildTools <jackisgood301@gmail.com>
2019-06-30 15:28:37 -0400

    add comments and expand code

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-var c = require('clipboardy')
-c.writeSync(require('profanity-util').purify(c.readSync())[0])
+const clipboard = require('clipboardy') // Clipboardy lets us read and write to the clipboard
+const pure = require('profanity-util').purify // Profanity Util lets us censor stuff
+clipboard.writeSync(pure(clipboard.readSync())[0]) // Replace the clipboard with the censored version
