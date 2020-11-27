e6a1b62a4e312c781c92e1ddcb7bfe0f8ca19aed
Jack <29169102+Jack5079@users.noreply.github.com>
2020-11-26 20:49:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const clipboard = require('clipboardy') // Clipboardy lets us read and write to the clipboard
+const clipboard = equire('clipboardy') // Clipboardy lets us read and write to the clipboard
 const { purify } = require('profanity-util') // Profanity Util lets us censor stuff
 clipboard.writeSync(purify(clipboard.readSync())[0]) // Replace the clipboard with the censored version
